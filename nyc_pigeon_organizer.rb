635fe81e741cb6edcabe9adfc42db82a62ed67c8
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2021-01-08 16:33:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,13 @@ require 'pry'
 
 
 def nyc_pigeon_organizer(data)
-new_pigeon
+ new_pigeon_list = {}
+ data.each do |col_gdr_liv, value|
+   value.each do |infos, total_names|
+     total_nameseach do|name|
+     if new_pigeon_list[name] == Nil 
+       new_pigeon_list [name] ={}
+     endif new_pigeon_list[name]{col_gdr_liv
+
   end
 
